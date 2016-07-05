f2b048871cc35ab1956c9e21da45f220a9bbff79
NearTox <NearTox@outlook.com>
2016-07-04 20:59:51 -0500

    added point

--- app/src/main/java/com/example/josefabioal/calculadora/MainActivity.java
+++ app/src/main/java/com/example/josefabioal/calculadora/MainActivity.java
@@ -8,33 +8,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements View.OnClickListener {
-    Toast mToast;
+    private Toast mToast;
+    private static int[] myIDArray = {
+            R.id.button0, R.id.button1, R.id.button2, R.id.button3, R.id.button4,
+            R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9,
+    };
     @Override
-    protected void onCreate(Bundle savedInstanceState)/*  */ {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
-        Button cero = (Button) findViewById(R.id.button0);
-        cero.setOnClickListener(this);
-        Button one = (Button) findViewById(R.id.button1);
-        one.setOnClickListener(this); // calling onClick() method
-        Button two = (Button) findViewById(R.id.button2);
-        two.setOnClickListener(this);
-        Button three = (Button) findViewById(R.id.button3);
-        three.setOnClickListener(this);
-        Button four = (Button) findViewById(R.id.button4);
-        four.setOnClickListener(this);
-        Button five = (Button) findViewById(R.id.button5);
-        five.setOnClickListener(this); // calling onClick() method
-        Button six = (Button) findViewById(R.id.button6);
-        six.setOnClickListener(this);
-        Button seven = (Button) findViewById(R.id.button7);
-        seven.setOnClickListener(this);
-        Button eight = (Button) findViewById(R.id.button8);
-        eight.setOnClickListener(this);
-        Button nine = (Button) findViewById(R.id.button9);
-        nine.setOnClickListener(this); // calling onClick() method
+        Button numero;
+        for(int i=0;i<myIDArray.length;i++) {
+            numero = (Button) findViewById(myIDArray[i]);
+            numero.setOnClickListener(this);
+        }
         Button point = (Button) findViewById(R.id.buttonPoint);
         point.setOnClickListener(this);
         Button equals = (Button) findViewById(R.id.buttonEquals);
@@ -60,7 +48,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         String str;
         switch (v.getId()) {
 
-            case R.id.button0:
+            case R.id.button0: {
                 str = output.getText().toString();
                 //because you cant add a 0 to a 0
                 //same case with -0
@@ -68,7 +56,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     output.append("0");
                 }
                 break;
-
+            }
             case R.id.button1:
             case R.id.button2:
             case R.id.button3:
@@ -100,6 +88,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 break;
             case R.id.buttonC:
                 output.setText("0");
+                break;
+            case R.id.buttonPoint:
+                    output.append(".");
                 break;
             default:
                 if(null != mToast){
